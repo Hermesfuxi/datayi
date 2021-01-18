eed0222a1140bb23070339f6c96fadceee70bae6
Hermesfuxi <tang2472854207@outlook.com>
2021-01-18 22:54:34 +0800

    feat: web和server项目构建

---
+++ datayi-server/src/main/java/bigdata/hermesfuxi/datayi/server/DatayiServerApplication.java
@@ -0,0 +1,13 @@
+package bigdata.hermesfuxi.datayi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatayiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatayiServerApplication.class, args);
+	}
+
+}
---
+++ datayi-server/src/test/java/bigdata/hermesfuxi/datayi/server/DatayiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bigdata.hermesfuxi.datayi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatayiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
